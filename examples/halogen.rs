ce8bf979e1b8358b3b885277e61186acd8ce3359
Andrew Grant <andrew.github@grantnet.us>
2026-05-30 17:41:17 -0400

    exp15

--- examples/halogen.rs
+++ examples/halogen.rs
@@ -397,12 +397,12 @@ fn custom_filter_pipeline(board: &Board, mv: viriformat::chess::chessmove::Move,
 
 macro_rules! net_id {
     () => {
-        "bullet-baseline"
+        "bullet-exp15"
     };
 }
 
 const NET_ID: &str = net_id!();
-const DATA_PATH: &str = "/data/300m.exp10.vf";
+const DATA_PATH: &str = "/data/400m.std.adv.vf";
 const CHECKPOINT_DIR: &str = concat!("/data/", net_id!());
 
 fn main() {
